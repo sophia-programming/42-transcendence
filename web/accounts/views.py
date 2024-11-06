876fa41d2e702f93576ba9ddbe2efd1fa7c3d474
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-11-06 15:06:42 +0900

    [add] accounts appにloginのテンプレートシステムを追加

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = "accounts/login.html"
