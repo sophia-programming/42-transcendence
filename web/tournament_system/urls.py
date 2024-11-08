98b32950d3f30ece5afe79013fa1b60fadbbeaa9
oaoba <hacchi0803.rera1126@gmail.com>
2024-11-09 01:47:57 +0900

    #5: [add] Webディレクトリにtournament page追加

--- web/tournament_system/urls.py
+++ web/tournament_system/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for core project.
+URL configuration for tournament_system project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from tournament_system import views  # views.py をインポート
 
 urlpatterns = [
-    path("tournament/", include("tournament_system.urls")),
-    path("accounts/", include("accounts.urls")),
-    path("admin/", admin.site.urls),
+    path(
+        "", views.tournament, name="tournament"
+    ),  # /tournament/ に対応するビューを指定
 ]
