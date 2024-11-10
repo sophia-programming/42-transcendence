221ced4d333e58bcf245705f5ad6fa5ae134fe08
oaoba <hacchi0803.rera1126@gmail.com>
2024-11-10 15:59:48 +0900

    #19: [fix] 重複したファイルを削除

--- web/core/urls.py
+++ web/core/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("tournament/matches/", include("matches.urls")),
+    path("tournament/matches/", include("tournament.urls")),
     path("tournament/", include("tournament.urls")),
     path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
--- web/matches/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "matches"
-urlpatterns = [
-    path("", views.matches_view, name="matches"),
-]
