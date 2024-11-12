9746ed7451f3d9f4afe4effd0e56bd1b01a89c4d
retakashi <119090016+retakashi@users.noreply.github.com>
2024-11-12 19:43:17 +0900

    [playground]:websocketテスト追加

---
+++ playground/websocket/gameplay/routing.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import consumers
+
+# websocket_urlpatterns = [
+#     path("ws/websocket/", consumers.WebsocketConsumer.as_asgi()),
+# ]
+
+websocket_urlpatterns = [
+    path(
+        "ws/websocket/", consumers.WebsocketConsumer.as_asgi()
+    ),  # gameplay用WebSocketルート
+]
