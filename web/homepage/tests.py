dc8d5b93d19f27354d1a3c4ed60ea1165703c3c2
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-11-12 20:16:44 +0900

    [update] ログイン必須に変更したため、homepageとgameplayのテストも変更

--- web/homepage/tests.py
+++ web/homepage/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase
-# Create your tests here.
 from django.urls import reverse
 from django.utils.html import escape
 
+from accounts.models import CustomUser
+
 
 class HomepageViewTests(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="testuser", password="password123"
+        )
+        self.client.login(username="testuser", password="password123")  # loginが必要
+
     def test_homepage_view(self):
         response = self.client.get(reverse("homepage"))
         self.assertEqual(response.status_code, 200)
