eac20cf9a95a237eabcdd93e7580d5baccd30977
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-11-12 20:11:25 +0900

    [update] トーナメントとゲームページもログイン必須に

--- web/gameplay/urls.py
+++ web/gameplay/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+
 from . import views
 
 app_name = "gameplay"
 urlpatterns = [
-    path('', views.playpage, name="gameplay"),
+    path("", views.playpage, name="gameplay"),
 ]
