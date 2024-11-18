5605be22884a327f2803a73159ccf238d0dd22c6
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-11-18 15:37:21 +0900

    [update] kibana apiを最新の書き方に

--- elk/create_index_pattern.sh
+++ elk/create_index_pattern.sh
@@ -3,20 +3,21 @@
 KIBANA_HOST="http://kibana:5601"
 KIBANA_USER="elastic"
 KIBANA_PASS=${KIBANA_PASSWORD}
-INDEX_PATTERN_ID="logstash-"
+INDEX_PATTERN_NAME="Logstash data view"
 INDEX_PATTERN_TITLE="logstash-*"
 
 cat <<EOF > index-pattern.json
 {
-  "attributes": {
+  "data_view": {
+    "name": "${INDEX_PATTERN_NAME}",
     "title": "${INDEX_PATTERN_TITLE}",
     "timeFieldName": "@timestamp"
   }
 }
 EOF
 
-curl -X POST "${KIBANA_HOST}/api/saved_objects/index-pattern/${INDEX_PATTERN_ID}" \
-  -H "kbn-xsrf: true" \
+curl -X POST "${KIBANA_HOST}/api/data_views/data_view" \
+  -H "kbn-xsrf: string" \
   -H "Content-Type: application/json" \
   -u ${KIBANA_USER}:${KIBANA_PASS} \
   --data @index-pattern.json
