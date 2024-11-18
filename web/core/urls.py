4726acb3f3268c1e70d0234edd8a20b263d98c8c
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-11-18 16:43:59 +0900

    [fix] conflict修正時のミスを修正

--- web/core/urls.py
+++ web/core/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-
 from homepage.views import homepage_view
 
 urlpatterns = [
@@ -26,6 +25,5 @@ urlpatterns = [
     path("gameplay/", include("gameplay.urls")),
     path("admin/", admin.site.urls),
     path("homepage/", homepage_view, name="homepage"),
-    path("homepage/", homepage, name="homepage"),
-    path('result/', include('resultpage.urls')),
+    path("result/", include("resultpage.urls")),
 ]
