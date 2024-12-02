da9dc0728ec03b0a67c96f4efd8ab0dcda5ae734
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-12-02 20:16:18 +0900

    [update] loginのurl nameからapiを無しに

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("api/login/", views.CustomLoginView.as_view(), name="api_login"),
+    path("api/login/", views.CustomLoginView.as_view(), name="login"),
     path("api/logout/", views.LogoutView.as_view(), name="logout"),
     path("api/signup/", views.SignUpView.as_view(), name="signup"),
     path("api/setup-otp/", views.SetupOTPView.as_view(), name="setup_otp"),
