5430d67fdff9000ea9f266adc69ae58ee36634eb
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-05 22:00:26 +0900

    [add]:admin.site.urls

--- playground/websocket_rtakashi/websocket_rtakashi/urls.py
+++ playground/websocket_rtakashi/websocket_rtakashi/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
+from django.contrib import admin
 # from django.urls import include, path
 
 # urlpatterns = [
@@ -37,6 +37,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
