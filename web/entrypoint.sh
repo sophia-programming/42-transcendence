399cc82289af7d696174d78418d2bdffaab25594
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-12-05 11:40:17 +0900

    [update] サーバー起動時に毎回dbをリフレッシュして{admin:admin}ユーザーをデフォルトで作るように

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -8,6 +8,9 @@ python manage.py migrate --noinput
 # todo: 入れる(staticディレクトリを作る)
 # python manage.py collectstatic --noinput
 
+python manage.py flush --noinput
+python manage.py loaddata fixtures/users.json
+
 # 環境変数のDEBUGの値がTrueの時はrunserverを、Falseの時はgunicornを実行
 if [ $DEBUG = "True" ]
 then
