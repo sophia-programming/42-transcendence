205e91a2394ede8795643e407a4e0df8a30292b1
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-06 20:19:31 +0900

    update:idを管理者ページで表示できるようにした

--- playground/websocket_rtakashi/websocket/admin.py
+++ playground/websocket_rtakashi/websocket/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import GameStatus   # 追加
+from .models import GameStatus
+class GameStatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status')
 
-admin.site.register(GameStatus)  # 追加
+admin.site.register(GameStatus, GameStatusAdmin)
