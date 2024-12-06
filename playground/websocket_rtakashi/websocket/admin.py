140fc0ae58c607c67747290374ab9b5cae8dd4bb
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-06 18:30:56 +0900

    update:ゲームの状態を管理するテーブル作成、管理サイトで操作できるようにした

--- playground/websocket_rtakashi/websocket/admin.py
+++ playground/websocket_rtakashi/websocket/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import GameStatus   # 追加
+
+admin.site.register(GameStatus)  # 追加
--- playground/websocket_rtakashi/websocket/models.py
+++ playground/websocket_rtakashi/websocket/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# status: init,stop,restart
+class GameStatus(models.Model):
+    status = models.CharField(max_length=10)
+
