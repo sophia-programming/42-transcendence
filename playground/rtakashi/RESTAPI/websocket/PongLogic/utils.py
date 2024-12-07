40cdf7a6d9cd6910fc32e0fcab412fc5d1a94aa0
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-07 22:40:27 +0900

    clean:REST APIディレクトリ整理

---
+++ playground/rtakashi/RESTAPI/websocket/PongLogic/utils.py
@@ -0,0 +1,22 @@
+import math
+
+
+class Utils:
+    def normalize_angle(angle):
+        return angle % (2 * math.pi)
+
+    def set_direction(ball):
+        if math.pi <= ball.angle and ball.angle <= math.pi * 2:
+            ball.direction["facing_up"] = True
+            ball.direction["facing_down"] = False
+        else:
+            ball.direction["facing_up"] = False
+            ball.direction["facing_down"] = True
+        if (math.pi / 2) <= ball.angle and ball.angle <= (math.pi * 3 / 2):
+            ball.direction["facing_left"] = True
+            ball.direction["facing_right"] = False
+        else:
+            ball.direction["facing_left"] = False
+            ball.direction["facing_right"] = True
+
+        # print("ball.direction_left: ", ball.direction["facing_left"])
