107fc3ddf9d1bb403efb310b8faea2e0afc6ea00
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-07 12:44:01 +0900

    mainブランチをマージ

--- playground/websocket_rtakashi/websocket/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import GameStatus
-class GameStatusAdmin(admin.ModelAdmin):
-    list_display = ('id', 'status')
-
-admin.site.register(GameStatus, GameStatusAdmin)
