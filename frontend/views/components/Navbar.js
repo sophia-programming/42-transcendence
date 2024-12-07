a337d385b0a062cdcb397e12a1b809ac76ce11ee
Kaistuc <kodaira-kai-vk@ynu.jp>
2024-12-07 20:05:09 +0900

    Navbarの位置修正

--- frontend/views/components/Navbar.js
+++ frontend/views/components/Navbar.js
@@ -33,22 +33,19 @@ const Navbar = {
                         </li>
                     </ul>
                     <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-i18n="language">
+                            Language
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-start" aria-labelledby="languageDropdown">
+                            <li><a class="dropdown-item" href="#" onclick="setLanguage('en')" data-i18n="english">English</a></li>
+                            <li><a class="dropdown-item" href="#" onclick="setLanguage('ja')" data-i18n="japanese">Japanese</a></li>
+                        </ul>
+                        </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#/login" data-i18n="login">Login</a>
+                            <a class="nav-link" href="#/login" data-i18n="login">Login</a>
                         </li>
                     </ul>
-                    <ul class="navbar-nav ms-auto">
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-i18n="language">
-                          Language
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="languageDropdown">
-                          <li><a class="dropdown-item" href="#" onclick="setLanguage('en')" data-i18n="english">English</a></li>
-                          <li><a class="dropdown-item" href="#" onclick="setLanguage('ja')" data-i18n="japanese">Japanese</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                    </div>
                 </div>
             </nav>`;
   },
