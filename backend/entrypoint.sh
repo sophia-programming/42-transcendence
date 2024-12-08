e8a3c1d004f8b1903f33a7c4ccba3ef763f346dd
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-12-08 23:27:17 +0900

    [update] 環境変数でprodとdevelopmentの設定を変え、prod環境ではhttps/wssで接続するように

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,8 +10,8 @@ python manage.py migrate --noinput
 python manage.py flush --noinput
 python manage.py loaddata fixtures/users.json
 
-# 環境変数のDEBUGの値がTrueの時はrunserverを、Falseの時はgunicornを実行
-if [ $DEBUG = "True" ]
+# 環境変数のENVIRONMENTの値がdevelopmentの時はrunserverを、Falseの時はdaphneを実行
+if [ $ENVIRONMENT = "development" ]
 then
     python manage.py runserver 0.0.0.0:8000
 else
