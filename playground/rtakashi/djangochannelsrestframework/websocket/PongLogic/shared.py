e22d7059698dee62fec3e2705f6f15f00c8a00d6
retakashi <119090016+retakashi@users.noreply.github.com>
2024-12-09 20:40:04 +0900

    fix:ゲーム画面、ボール、パドルクラスをシングルトンにした

--- playground/rtakashi/djangochannelsrestframework/websocket/PongLogic/shared.py
+++ playground/rtakashi/djangochannelsrestframework/websocket/PongLogic/shared.py
@@ -11,32 +11,41 @@ class Singleton:
 
 
 class game_window(Singleton):
-    width = 1000
-    height = 600
+    def __init__(self):
+        if not hasattr(self, "initialized"):
+            self.width = 1000
+            self.height = 600
+            self.initialized = True
 
 
 class ball(Singleton):
-    radius = 10
-    x = 500
-    y = 300
-    angle = 0
-    velocity = 5
-    direction = {
-        "facing_up": False,
-        "facing_down": False,
-        "facing_right": False,
-        "facing_left": False,
-    }
-    bound_angle = {
-        "left_top": math.pi * 7 / 4,
-        "left_bottom": math.pi / 4,
-        "right_top": math.pi * 5 / 4,
-        "right_bottom": math.pi * 3 / 4,
-    }
+    def __init__(self):
+        if not hasattr(self, "initialized"):
+            self.radius = 10
+            self.x = 500
+            self.y = 300
+            self.angle = 0
+            self.velocity = 5
+            self.direction = {
+                "facing_up": False,
+                "facing_down": False,
+                "facing_right": False,
+                "facing_left": False,
+            }
+            self.bound_angle = {
+                "left_top": math.pi * 7 / 4,
+                "left_bottom": math.pi / 4,
+                "right_top": math.pi * 5 / 4,
+                "right_bottom": math.pi * 3 / 4,
+            }
+            self.initialized = True
 
 
 class paddle(Singleton):
-    width = 15
-    height = 120
-    left_y = 240
-    right_y = 240
+    def __init__(self):
+        if not hasattr(self, "initialized"):
+            self.width = 15
+            self.height = 120
+            self.left_y = 240
+            self.right_y = 240
+            self.initialized = True
