27d3f803f43a7146298df7b43f56cc18dee74218
oaoba <hacchi0803.rera1126@gmail.com>
2024-12-13 20:42:22 +0900

    #110: [add] RecordMatchViewクラスを作成し、POSTリクエストで試合結果を受け取れるようにした

--- backend/tournament/views.py
+++ backend/tournament/views.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from rest_framework.views import APIView
+from .serializers import MatchSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class TournamentView(LoginRequiredMixin, TemplateView):
     template_name = "tournament/tournament.html"
+
+
+class RecordMatchView(APIView):
+    def post(self, request):
+        serializer = MatchSerializer(data=request.data)
+        if serializer.is_valid():
+            # ここでデータを保存する処理を書く
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
