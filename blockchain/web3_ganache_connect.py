a3b754446fbb2e881247841e87b95c0d84031ae1
oaoba <hacchi0803.rera1126@gmail.com>
2024-12-16 22:18:49 +0900

    #90: [ok] test

--- blockchain/web3_ganache_connect.py
+++ blockchain/web3_ganache_connect.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 import json
+import os
 
 # Ganacheのサービスに接続
 ganache_url = "http://ganache:8545"
@@ -12,15 +13,29 @@ else:
     print("Failed to connect to Ganache")
 
 # アカウントと秘密鍵の設定(将来的に自動でとってこれるようにしたい)
-account_0 = web3.to_checksum_address('0xfD089344cd7cB5890A8b14D5761C709F40691A40')
-private_key = '0x410e2482b0e56cf888d67642a9aef8f3e355759bd116074e5f254f7558e55e36'
+account_0 = web3.to_checksum_address('0xdE0C1d0ac10c94c344AB5ebA66f5B685c4C9a18c')
+private_key = '0xc90093bb19a01812b75ccf5fe1288f7689ab5c304179f76f232c1baa7f4148b4'
 
-# デプロイされたコントラクトのアドレスとABIを設定
-contract_address = "0x5ba69AA483B8575509F005ECb7e2Fdf2715F72ca"
-abi_path = "./blockchain/truffle/build/contracts/Tournament.json"
-with open(abi_path, 'r') as f:
-    contract_json = json.load(f)  # JSON文字列をPythonの辞書型に変換
-    abi = contract_json['abi']    # コントラクトのABIのみを取り出す
+# デプロイされたコントラクトのアドレスを取得
+contract_address = "0x73Be9c27bda8E9dD9fFd9913d79302bc15c4D5CF"
+
+abi_path = "blockchain/truffle/build/contracts/Tournament.json"
+
+# ファイルの存在確認
+if not os.path.exists(abi_path):
+    raise FileNotFoundError(f"指定されたファイルが見つかりません: {abi_path}")
+
+try:
+    with open(abi_path, 'r') as f:
+        contract_json = json.load(f)  # JSONファイルを読み込む
+except json.JSONDecodeError as e:
+    raise ValueError(f"JSONの読み込みに失敗しました: {e}")
+
+# 'abi'キーの存在確認
+if 'abi' not in contract_json:
+    raise KeyError("'abi'キーがJSON内に存在しません")
+
+abi = contract_json['abi']  # コントラクトのABIを取得
 
 # デプロイされたコントラクトのインスタンスを作成
 tournament = web3.eth.contract(address=contract_address, abi=abi)
