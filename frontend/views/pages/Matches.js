d9052989bbf5ae28c9054d0165066facb290a559
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-12-18 00:54:07 +0900

    [update] トーナメントのデータ構造の変更に伴ってマッチページを修正

--- frontend/views/pages/Matches.js
+++ frontend/views/pages/Matches.js
@@ -23,24 +23,22 @@ const Matches = {
 function updateMatchDisplay(tournament) {
   // Round 1 matches
   tournament.matches.forEach((match) => {
-    if (match.player_matches.length === 2) {
-      const player1 = match.player_matches[0].player.name;
-      const player2 = match.player_matches[1].player.name;
-      const score1 = match.player_matches[0].score;
-      const score2 = match.player_matches[1].score;
+    const player1 = match.player1.name;
+    const player2 = match.player2.name;
+    const score1 = match.player1_score;
+    const score2 = match.player2_score;
 
-      const matchNumber = match.match_number;
-      if (matchNumber <= 4) {
-        document.querySelector(`#match${matchNumber}-player1`).textContent =
-          player1;
-        document.querySelector(`#match${matchNumber}-player2`).textContent =
-          player2;
-        if (score1 > 0 || score2 > 0) {
-          document.querySelector(`#match${matchNumber}-score1`).textContent =
-            score1;
-          document.querySelector(`#match${matchNumber}-score2`).textContent =
-            score2;
-        }
+    const matchNumber = match.match_number;
+    if (matchNumber <= 4) {
+      document.querySelector(`#match${matchNumber}-player1`).textContent =
+        player1;
+      document.querySelector(`#match${matchNumber}-player2`).textContent =
+        player2;
+      if (score1 > 0 || score2 > 0) {
+        document.querySelector(`#match${matchNumber}-score1`).textContent =
+          score1;
+        document.querySelector(`#match${matchNumber}-score2`).textContent =
+          score2;
       }
     }
   });
