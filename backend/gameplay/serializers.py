17a6cae076470e53a732ddc6cf0e769f8a16bb93
Kaistuc <kodaira-kai-vk@ynu.jp>
2024-12-18 21:07:10 +0900

    PUT(GETはデフォルト)のみ実装

--- backend/gameplay/serializers.py
+++ backend/gameplay/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
 from .models import GameSetting
 
+# class GameSettingSerializer(serializers.ModelSerializer):
+# 	class Meta:
+# 		model = GameSetting
+# 		fields = '__all__'
+
 class GameSettingSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = GameSetting
-		fields = '__all__'
+    class Meta:
+        model = GameSetting
+        fields = ['ball_velocity', 'ball_size', 'map']
+        
+	# GETはデフォルトで実装されている
+
+    def update(self, instance, validated_data):
+        """
+        PUTメソッド用の更新ロジック
+        """
+        # データを更新(データがない場合は元のデータを使う)
+        instance.ball_velocity = validated_data.get('ball_velocity', instance.ball_velocity)
+        instance.ball_size = validated_data.get('ball_size', instance.ball_size)
+        instance.map = validated_data.get('map', instance.map)
+        instance.save()
+        # 更新したインスタンスを返す
+        return instance
