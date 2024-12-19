57eee3c579ee94cd7bceec520daeb2ba05f50c93
oaoba <hacchi0803.rera1126@gmail.com>
2024-12-20 01:08:14 +0900

    #135: [add] urls.pyにエンドポイントを追加

--- backend/tournament/urls.py
+++ backend/tournament/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import TournamentRegisterView
+from .views import SaveScoreView
 
 app_name = "tournament"
 urlpatterns = [
@@ -9,4 +10,10 @@ urlpatterns = [
         TournamentRegisterView.as_view(),
         name="tournament-register",
     ),
+    path(
+        "api/save-score/",
+        SaveScoreView.as_view(),
+        name="save-score",
+    ),
 ]
+
